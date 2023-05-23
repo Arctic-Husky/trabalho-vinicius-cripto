bc833ac226e5050c24bbe13027f845988f6b8130
Caio Schmidt <caiofonseca6@gmail.com>
2023-05-23 20:34:38 -0300

    Add: add project

---
+++ TrabalhoCriptomoeda/Model/CriptoCoin.cs
@@ -0,0 +1,26 @@
+﻿
+namespace TrabalhoCriptomoeda.Model;
+
+public class CriptoCoin
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public decimal Price { get; set; }
+
+    public CriptoCoin()
+    {
+        Name = "";
+        Description = "";
+        Price = 0;
+    }
+
+    public CriptoCoin(string name, string description, decimal price)
+    {
+        Name = name;
+        Description = description;
+        Price = price;
+    }
+
+}
+
